ef02a2aaa2d82a737ed9eb3d5c712f464c63db4a
22TalhaFaiz <138751622+22TalhaFaiz@users.noreply.github.com>
2023-11-21 01:09:53 +0500

    ss

--- New folder/profile.php
+++ New folder/profile.php
@@ -16,18 +16,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </head>
 <body>
-    <form class="form w-50 mt-5 mx-auto p-3 bg-light rounded-2" action="edituser.php" method="POST">
-        <h1 class="text-center display-5">Edit User</h1>
+    
+    <form class="pform w-50 mt-5 mx-auto p-3  rounded-2" action="edituser.php" method="POST" >
+        <h1 class="text-center ">Edit User</h1>
         <input type="hidden" value="<?php echo $user["id"]?>" name="id"/>
         <input value="<?php echo $user["user_name"]?>"  placeholder="Enter username" class="form-control p-1 mt-2" type="text" name="user_name">
         <input value="<?php echo $user["user_email"]?>"  placeholder="Enter email" class="form-control p-1 mt-2" type="text" name="user_email">
         <input value="<?php echo $user["user_password"]?>"  placeholder="Enter password" class="form-control p-1 mt-2" type="password" name="user_password">
         <input value="<?php echo $user["user_address"]?>"  placeholder="Enter Address" class="form-control p-1 mt-2" type="text" name="user_address">
-        <select name="role_id" id="" class="form-control mt-2">
+        <select type="hidden" name="role_id" id="" class="form-control mt-2" >
             <?php
                 while($data = mysqli_fetch_assoc($result)){
             ?>
@@ -36,7 +35,8 @@
                 }
             ?>
         </select>
-        <input class="btn btn-warning w-100 mt-5" type="submit" name="submit">
+        <div class="text-center  mt-5"><button class ="sbtn w-100" type="submit" name="submit">Submit</button></div>
+        
     </form>
 </body>
 </html>
